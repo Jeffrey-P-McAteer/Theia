d452756caedf790d23d3467787605399257017c8
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2019-12-08 17:45:04 -0500

    discovered I can just embed YT directly instead of relying on 15k lines of lua to parse 150k lines of JS. Neat.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
-use vlc::{Instance, Media, MediaPlayer};
+
 use std::thread;
 
 fn main() {
-    // Create an instance
+    //libvlc_experiment();
+    gui_experiment();
+}
+
+fn gui_experiment() {
+	use web_view::*;
+
+	web_view::builder()
+        .title("theia")
+        .content(Content::Url( "https://www.youtube.com/embed/L3ug_ZRNJiw?rel=0&autoplay=1" ))
+        .size(800, 600)
+        .resizable(true)
+        .debug(true)
+        .user_data(())
+        .invoke_handler(|_webview, _arg| Ok(()))
+        .run()
+        .unwrap();
+
+}
+
+fn libvlc_experiment() {
+	use vlc::{Instance, Media, MediaPlayer};
+
+	// Create an instance
     let instance = Instance::new().unwrap();
     
     // Create a media from a file
     
     //let md = Media::new_path(&instance, "/j/downloads/trailer_400p.ogg").unwrap();
-    //let md = Media::new_location(&instance, "https://www.youtube.com/watch?v=Y226GeS8iAg").unwrap();
+    //let md = Media::new_location(&instance, "https://www.youtube.com/watch?v=L3ug_ZRNJiw").unwrap();
 	let md = Media::new_location(&instance, "https://download.blender.org/peach/trailer/trailer_400p.ogg").unwrap();
     
     // Create a media player
@@ -19,6 +42,6 @@ fn main() {
     mdp.play().unwrap();
 
     // Wait for 10 seconds
-    thread::sleep(::std::time::Duration::from_secs(15));
+    thread::sleep(::std::time::Duration::from_secs(30));
 }
 
